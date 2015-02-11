3a0ae4becf71fe63b4f1a58b824bd916e34145e6
Federica <eotchi@gmail.com>
2015-02-11 10:05:45 +0100

    Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vapebox::Application.config.secret_token = '5cae3cb0811aa81a4d109b25da62a14fa7e017ace44ea4ea436b45b35e9c7ceb0d11ebc5c21c7700595c0a70e7ca3d28bd229a46ee07d5b16c0295f606db89e0'
