b13db5d88afe95646d49b732cb9b5b3708ebfc4e
Federica <eotchi@gmail.com>
2015-02-17 14:18:13 +0100

    Test file revised

--- test/unit/liquid_test.rb
+++ test/unit/liquid_test.rb
@@ -5,21 +5,22 @@ class LiquidTest < ActiveSupport::TestCase
   test "A liquid can be saved, if name, base and nicotine are given" do
     liquid = Liquid.new(name: "Strawberry Cheesecake", base:"PG/VG", nicotine:9, notes:'This is not bad at all')
     assert liquid.valid?
+#    assert_equal "The liquid wasn't saved", liquid.errors[:name].first
   end
   
   test "A liquid should always have a name " do
-     liquid = Liquid.new(base: "PG", nicotine: 9)
-     assert !liquid.valid?
+    liquid = Liquid.new(base: "PG", nicotine: 9)
+    assert !liquid.valid?, liquid.errors[:name].first
   end
 
   test "A liquid should always have a defined base type " do
-     liquid = Liquid.new(name: "Strawberry Cheescake", nicotine: 9)
-     assert !liquid.valid?
+    liquid = Liquid.new(name: "Strawberry Cheescake", nicotine: 9)
+    assert !liquid.valid?, liquid.errors[:base].first
   end
 
   test "A liquid should always have a nicotine amount " do
-     liquid = Liquid.new(name: "Strawberry Cheescake", base: "PG")
-     assert !liquid.valid?
+    liquid = Liquid.new(name: "Strawberry Cheescake", base: "PG")
+    assert !liquid.valid?, liquid.errors[:nicotine].first
   end
 
 end
